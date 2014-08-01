777e0f6ed11f7283c52152c09af6290818b4fbc3
thomas <thomas@vaadin.com>
2014-08-01 11:01:01 +0300

    edit improvements

---
+++ src/com/thomas/winecellar/ui/Stepper.java
@@ -0,0 +1,74 @@
+package com.thomas.winecellar.ui;
+
+import com.vaadin.server.FontAwesome;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.CustomField;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+
+public class Stepper extends CustomField<Integer> {
+
+	private static final long serialVersionUID = -6591416612233324201L;
+
+	private final Label currentValue = new Label();
+
+	public Stepper(int initial) {
+		setValue(initial);
+		setSizeUndefined();
+		setImmediate(true);
+		setStyleName("stepper");
+	}
+
+	@Override
+	public Class<Integer> getType() {
+		return Integer.class;
+	}
+
+	@Override
+	protected Component initContent() {
+		final Button minus = new Button(FontAwesome.MINUS);
+		final Button plus = new Button(FontAwesome.PLUS);
+
+		currentValue.setValue(getValue() + "");
+		currentValue.setWidth("100px");
+
+		final HorizontalLayout root = new HorizontalLayout(minus, currentValue,
+				plus);
+		root.setSpacing(true);
+		root.setComponentAlignment(currentValue, Alignment.MIDDLE_CENTER);
+
+		minus.addClickListener(new ClickListener() {
+
+			private static final long serialVersionUID = -198862221678984094L;
+
+			@Override
+			public void buttonClick(ClickEvent event) {
+				if (getValue() > 0) {
+					setValue(getValue() - 1);
+				}
+			}
+		});
+		plus.addClickListener(new ClickListener() {
+
+			private static final long serialVersionUID = -198862221678984094L;
+
+			@Override
+			public void buttonClick(ClickEvent event) {
+				setValue(getValue() + 1);
+			}
+		});
+
+		return root;
+	}
+
+	@Override
+	protected void setInternalValue(Integer newValue) {
+		super.setInternalValue(newValue);
+		currentValue.setValue(String.valueOf(getValue()));
+	}
+
+}
---
+++ src/com/thomas/winecellar/ui/iphone/WineToolbar.java
@@ -0,0 +1,44 @@
+package com.thomas.winecellar.ui.iphone;
+
+import com.thomas.winecellar.ui.WinePresenter;
+import com.vaadin.addon.touchkit.ui.Toolbar;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+
+public class WineToolbar extends Toolbar {
+
+	private static final long serialVersionUID = -5424019068247983406L;
+
+	public WineToolbar(final WinePresenter presenter) {
+
+		final Button add = new Button("Add wine");
+		add.setIcon(FontAwesome.PLUS);
+		addComponent(add);
+
+		add.addClickListener(new ClickListener() {
+
+			private static final long serialVersionUID = -9043716252613242039L;
+
+			@Override
+			public void buttonClick(ClickEvent event) {
+				presenter.addClicked();
+			}
+		});
+
+		final Button search = new Button("Search");
+		search.setIcon(FontAwesome.SEARCH);
+		addComponent(search);
+
+		search.addClickListener(new ClickListener() {
+
+			private static final long serialVersionUID = -9043716252613242039L;
+
+			@Override
+			public void buttonClick(ClickEvent event) {
+				presenter.searchClicked();
+			}
+		});
+	}
+}
